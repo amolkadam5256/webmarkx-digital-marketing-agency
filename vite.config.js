671f38d22fed1dab5eabdf46009b66b6725b43ed
amolkadam5256 <amolkadam1274@gmail.com>
2025-08-13 17:45:17 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+  
     tailwindcss(),
   ],
-  server: {
-    // Fix React Router refresh issue
-    historyApiFallback: true,
-  },
-  build: {
-    // Ensure correct paths for production
-    rollupOptions: {
-      input: '/index.html',
-    },
-  },
-});
+})
