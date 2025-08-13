259972788b735ff7bdb4007ded7f200a8ee4fddc
amolkadam5256 <amolkadam1274@gmail.com>
2025-08-13 13:12:22 +0530

    Added Some Code

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-        <h1 class="text-3xl font-bold underline text-red-900">
-    Hello world!
-  </h1>
+      <h1 class="text-3xl font-bold underline text-red-900">
+        Hello world!
+      </h1>
 
     </div>
   )
